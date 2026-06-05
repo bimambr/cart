3b1c3bf6199fa2dc1f5d8b68d05e9d89797daa22
Muhammad Bima Ramadhan <34125207+bimambr@users.noreply.github.com>
2026-06-05 09:42:17 +0700

    chore: bump copyright year to 2026

--- lib.py
+++ lib.py
@@ -1,5 +1,5 @@
 """
-Copyright 2025 Muhammad Bima Ramadhan
+Copyright 2026 Muhammad Bima Ramadhan
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
 files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy,
